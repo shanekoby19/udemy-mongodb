75c1c736f75a6ac35cb86013b7913205ba933f1c
Shane Kobylecky <skobylecky1@gmail.com>
2023-02-15 07:12:35 -0800

    Add notes for the  operator

--- Section 12 - Aggregation Framework/commands.js
+++ Section 12 - Aggregation Framework/commands.js
@@ -1,5 +1,59 @@
 db.persons.aggregate([
-    { $match: { gender: 'female' } },
-    { $group: { _id: { state: "$location.state" }, totalPersons: { $sum: 1 } } },
-    { $sort: { totalPersons: -1 } }
+    { 
+        $project : { 
+            _id: 0, 
+            gender: 1, 
+            fullName: { 
+                $concat: [
+                    { $toUpper: { $substrCP: ["$name.first", 0, 1] } },
+                    { $substrCP: ["$name.first", 1, { $subtract: [{ $strLenCP:  "$name.first" }, 1]}] },
+                    " ", 
+                    { $toUpper: { $substrCP: ["$name.last", 0, 1] } },
+                    { $substrCP: [ "$name.last", 1, { $subtract: [{ $strLenCP: "$name.last" }, 1]} ]}
+                ]
+            },
+            geoJSON: {
+                type: 'Point',
+                coordinates: [
+                    { 
+                        $convert: {
+                            input: "$location.coordinates.latitude", 
+                            to: "double",
+                            onError: 0.0,
+                            onNull: 0.0,
+                        }
+                    },
+                    {
+                        $toDouble: "$location.coordinates.longitude"
+                    }
+                ]
+            },
+            email: 1,
+            age: "$dob.age",
+            birthday: {
+                $dateToString: {
+                    date: { 
+                        $toDate: "$dob.date"
+                    },
+                    format: "%m-%d-%Y"
+                }
+                
+            }
+        }
+    },
+    {
+        $group: {
+            _id: { 
+                "birthYear": { 
+                    $substrCP: [
+                        "$birthday", 
+                        { 
+                            $subtract: [{ $strLenCP: "$birthday"}, 4] 
+                        },
+                        4
+                    ]
+                }
+            }
+        }
+    }
 ])
