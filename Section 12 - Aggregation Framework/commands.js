4b15226cfcd2232a900f54273dc11fdf581b1afc
Shane Kobylecky <skobylecky1@DESKTOP-N059GK8.ClarkCoHS.local>
2023-02-22 16:36:55 -0800

    Add additional notes on the aggregation pipeline and learn how to write db.collection.aggregate() methods. git push

--- Section 12 - Aggregation Framework/commands.js
+++ Section 12 - Aggregation Framework/commands.js
@@ -57,3 +57,121 @@ db.persons.aggregate([
         }
     }
 ])
+
+
+
+db.friends.aggregate([
+    {
+        $unwind: "$hobbies"
+    },
+    {
+        $group: { _id: { age: "$age"} , hobbies: { $addToSet: "$hobbies" }}
+    }
+])
+
+db.friends.aggregate([
+    {
+        $project: { 
+            _id: 0,
+            numScores: { 
+                $size: "$examScores" 
+            }
+        }
+    }
+])
+
+db.friends.aggregate([
+    {
+        $project: {
+            _id: 0,
+            examScores: {
+                $filter: {
+                    input: "$examScores",
+                    as: "examScore",
+                    cond: {
+                        $gt: ["$$examScore.score", 60]
+                    }
+                }
+            }
+        }
+    }
+])
+
+db.friends.aggregate([
+    {
+        $unwind: "$examScores"
+    },
+    { 
+        $project: {
+            _id: 1,
+            name: 1,
+            age: 1,
+            score: "$examScores.score"
+        }
+    },
+    {
+        $sort: {
+            score: -1
+        }
+    },
+    {
+        $group: {
+            _id: "$_id",
+            name: {
+                $first: "$name"
+            },
+            maxScore: {
+                $max: "$score"
+            }
+        }
+    },
+    {
+        $sort: {
+            maxScore: -1
+        }
+    }
+])
+
+
+db.persons.aggregate([
+    {
+        $bucket: {
+            groupBy: "$dob.age",
+            boundaries: [18, 30, 42, 54, 66, 78],
+            output: {
+                numPersons: { $sum: 1 },
+                averageAge: { $avg: "$dob.age" },
+            }
+        }
+    }
+])
+
+db.persons.aggregate([
+    {
+        $match: {
+            "gender": "male"
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            name: { $concat: ["$name.title", " ", "$name.first", " ", "$name.last"]},
+            gender: 1,
+            birthdate: { $toDate: "$dob.date" }
+        }
+    },
+    {
+        $sort: {
+            birthdate: 1
+        }
+    },
+    { 
+        $skip: 10
+    },
+    {
+        $limit: 10
+    },
+    {
+        $out: "sortedAges"
+    }
+])
